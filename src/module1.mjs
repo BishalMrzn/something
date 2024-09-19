0f1ef463f21a355982c761eb602b6a283b14c5da
BishalMrzn <hapiebishal5@gmail.com>
2024-09-19 22:33:27 +0545

    modules

---
+++ src/module1.mjs
@@ -0,0 +1,7 @@
+const a="Bishal";
+const b="Sarin";
+const c="Mahendra";
+
+export default c;
+export {a};
+export {b};
---
+++ src/module2.mjs
@@ -0,0 +1,4 @@
+import uz,{a,b} from './module1.mjs'
+console.log(uz)
+console.log(b)
+console.log(a)
