6bbab06b67f84ec4465a3b6db92010b9c237c288
BishalMrzn <hapiebishal5@gmail.com>
2024-10-01 22:48:28 +0545

    updated

--- src/App.js
+++ src/App.js
@@ -9,9 +9,12 @@ function App() {
   const togglebtn=()=>{
     if(mode==='dark'){
     setMode('light')
+    document.body.style.backgroundColor='white'
   }
 else{
+  
   setMode('dark')
+  document.body.style.backgroundColor='#000023'
 }}
   const [mode,setMode]=useState('dark')
   return (
@@ -19,7 +22,7 @@ else{
      <Navbar mode={mode} togglebtn={togglebtn}/>
      <div className="container">
 
-     <Textform heading="Change your text below"/>
+     <Textform heading="Change your text below" mode={mode}/>
      {/* <Accordian heading="My Accordian"/> */}
      </div>
     </>
--- src/component/Textform.js
+++ src/component/Textform.js
@@ -17,21 +17,25 @@ export default function Textform(props) {
   const handleOnChange=(event)=>{
     setText(event.target.value);
   }
-    const [text,setText]=useState("Enter your text");
+    const [text,setText]=useState("");
     // const [bolded,notbolded]=useState(false);
 
     
   return (
-    <div>
+    <div className='container' style={{color:props.mode==='light'?'#000023':'white'}}>
         <h1>{props.heading}</h1>
 <div className="mb-3">
-<textarea className="form-control" value={text} onChange={handleOnChange} id="exampleFormControlTextarea1" rows="8"></textarea>
+<textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor:props.mode==='dark'?'#000023':'white',color:props.mode==='light'?'#000023':'white'}} id="exampleFormControlTextarea1" rows="8"></textarea>
 <button className="btn btn-primary" onClick={handleUpCLick}>Change to upperCase</button>
 <button className="btn btn-primary" onClick={handleDownCLick}>Change to LowerCase</button>
 <button className="btn btn-primary" onClick={handleSpace}>remove space</button>
 
 </div>
 
+<div className="container"style={{color:props.mode==='light'?'#000023':'white'}}>
+  <h2>Preview</h2>
+  <p>{text.length>0 ?text:'Enter the text to preview'}</p>
+</div>
     </div>
   )
 }
